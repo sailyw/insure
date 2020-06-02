c1b68bb016ed251d6716cfb30d5a5004f76930a0
sailyw <1152827086@qq.com>
2020-06-02 10:38:42 +0800

    update infoSearch

--- src/views/InfoSearch.vue
+++ src/views/InfoSearch.vue
@@ -19,23 +19,27 @@
     <main>
       <div class="insure-search">
         <el-form :inline="true" :model="formInline" class="demo-form-inline">
-          <el-form-item label="审批人">
-            <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+          <el-form-item label="保单号：">
+            <el-input v-model="formInline.policyno"></el-input>
           </el-form-item>
-          <el-form-item label="活动区域">
-            <el-select v-model="formInline.region" placeholder="活动区域">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
-            </el-select>
+          <el-form-item label="临时报案号:">
+            <el-input v-model="formInline.temporaryno"></el-input>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="onSubmit">查询</el-button>
           </el-form-item>
         </el-form>
+      <div class="tablecase">
+        <el-table :data="tableData" stripe style="width: 100%" :header-cell-style="{background:'#eef1f6'}" border>
+           <el-table-column prop="kong" label="" width="180"></el-table-column>
+          <el-table-column prop="caseState" label="按键状态" width="180"></el-table-column>
+          <el-table-column prop="temporaryno" label="临时报案号" width="180"></el-table-column>
+          <el-table-column prop="policyno" label="保单号" width="180"></el-table-column>
+           <el-table-column prop="kong" label="" width="180"></el-table-column>
+        </el-table>
       </div>
-      
+      </div>
     </main>
-
   </div>
 </template>
 
@@ -43,52 +47,82 @@
 export default {
   name: "InfoSearch",
   data() {
-      return {
-        formInline: {
-          user: '',
-          region: ''
+    return {
+      formInline: {
+        policyno: "",
+        temporaryno: ""
+      },
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
         }
-      }
-    },
-    methods: {
-      onSubmit() {
-        console.log('submit!');
-      }
+      ]
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
     }
+  }
 };
 </script>
 <style lang="less" scoped>
 @import "../assets/base.less";
-.navBtn {
-  color: #f8f8f8;
-  background: #333;
-  width: 100%;
-  height: 50px;
-  ul {
-    display: flex;
-    li {
-      display: inline-block;
-      height: 50px;
-      flex: 1;
-      text-align: center;
-      line-height: 50px;
-      p {
-        color: #f8f8f8;
-        font-size: 14px;
-      }
-      p.selectbtn {
-        color: orange;
+#registration{
+  // min-height: 100vh;
+  .navBtn {
+    color: #f8f8f8;
+    background: #333;
+    width: 100%;
+    height: 50px;
+    ul {
+      display: flex;
+      li {
+        display: inline-block;
+        height: 50px;
+        flex: 1;
+        text-align: center;
+        line-height: 50px;
+        p {
+          color: #f8f8f8;
+          font-size: 14px;
+        }
+        p.selectbtn {
+          color: orange;
+        }
       }
     }
   }
-}
-main{
-  background-color: #f0eff4;
-  padding: 8px;
-  .insure-search{
-    margin-top: 50px;
-    background-color: #fff;
+  main {
+    background-color: #f0eff4;
     padding: 8px;
+    min-height: 100vh;
+    .insure-search {
+      margin-top: 50px;
+      background-color: #fff;
+      padding: 8px;
+    }
+    .tablecase{
+      background: #ffffff;
+      margin-top: 15px;
+    }
   }
 }
 </style>
